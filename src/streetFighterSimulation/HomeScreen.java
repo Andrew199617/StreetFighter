6fc2f842c82b1dc547c3a36d76acde10514514cd
Andrew Velez <andrewmauro199617@yahoo.com>
2015-03-03 16:48:08 -0700

    First Version

---
+++ src/streetFighterSimulation/HomeScreen.java
@@ -0,0 +1,13 @@
+package streetFighterSimulation;
+
+import greenfoot.World;
+
+public class HomeScreen extends World {
+
+	private static final int WORLDWIDTH = 1440, WORLDHEIGHT = 810;
+	
+	public HomeScreen(int worldWidth, int worldHeight, int cellSize) {
+		super(WORLDWIDTH, WORLDHEIGHT, cellSize);
+	}
+
+}
