262f043e62abba9d5cda75bd77e8192ba751d4d4
Andrew Velez <andrewmauro199617@yahoo.com>
2015-03-04 16:50:29 -0700

    asd

--- src/streetFighterSimulation/Background.java
+++ src/streetFighterSimulation/Background.java
@@ -7,11 +7,6 @@ public class Background extends Actor {
 
 	public Background (String s){
 		GreenfootImage image = new GreenfootImage(s);
-<<<<<<< HEAD
 		this.setImage(image);
-=======
-		System.out.println("Hello");
->>>>>>> 9bc3724b94dfdf453418b108b79d3ae6ef09df9d
 	}
-	
 }
