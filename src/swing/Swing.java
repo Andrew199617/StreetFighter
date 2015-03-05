7bc4bef4f818c7481bcf3a6102e1f07c1b0a64c9
Connor <connor@a440hz.com>
2015-03-05 16:46:23 -0700

    Basis Swing App, (Doesn't work right now.)

--- src/swing/Swing.java
+++ src/swing/Swing.java
@@ -1,6 +1,35 @@
 package swing;
 
-public class Swing {
+import javax.swing.*;
+
+public class Swing extends JFrame {
+	
+	
+	public static void main(String[]args){
+		MySwingTry first = new MySwingTry();
+		first.setTitle("First try");
+		first.setSize(300,200);
+		first.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		first.setVisible(true);
+		
+	}
+	
+	
+	JPanel mypanel;
+	JButton mybutton;
+	JLabel mylabel;
+	
+	public void MySwingTry(){
+		mypanel = new JPanel();
+		mybutton = new JButton("OK");
+		mylabel = new JLabel();
+		
+		mypanel.add(mybutton);
+		mypanel.add(mylabel);
+		this.add(mypanel);
+	}
+	
+	
 
 	//test
 }
