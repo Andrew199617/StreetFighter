81d365513fa454e42be90b900c7d279d4c04af38
David Leiter <davidleiter1@gmail.com>
2015-03-05 15:01:30 -0700

    Animation

--- src/streetFighterSimulation/HomeScreen.java
+++ src/streetFighterSimulation/HomeScreen.java
@@ -6,10 +6,13 @@ public class HomeScreen extends World {
 
 	private static final int WORLDWIDTH = 1440, WORLDHEIGHT = 810;
 	
+	Player player = new Player();
+	
 	public HomeScreen() {
 		super(WORLDWIDTH, WORLDHEIGHT, 1);
 		this.addObject(new Background("image/Map_1.png"), WORLDWIDTH/2, WORLDHEIGHT/2);
 		this.addObject(new Button("image/Dragon_Stage.gif"), WORLDWIDTH/2, WORLDHEIGHT/2);
+		addObject(player, WORLDWIDTH/10, WORLDHEIGHT-WORLDHEIGHT/10);
 	}
 
 }
