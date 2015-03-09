5ba769db07c6e50c92a638801dd2b7c92d6a1499
David Leiter <davidleiter1@gmail.com>
2015-03-09 14:30:20 -0600

    Players move passed each other and turn

--- src/charcter/Raptor.java
+++ src/charcter/Raptor.java
@@ -4,9 +4,13 @@ import greenfoot.Greenfoot;
 import greenfoot.GreenfootImage;
 
 public class Raptor extends Player{
+	
+	private int animate = 0;
+	private static final int MAX_JUMP = 25;
+	public boolean maxed = false;
 
 	public Raptor(){
-		super (5,8, Character.RAPTOR);
+		super (6, 8, "up", Character.RAPTOR);
 	}
 
 }
