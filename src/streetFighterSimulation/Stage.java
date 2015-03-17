299ce1be5e6d12f61d93f3d1e63322756dc24101
David Leiter <davidleiter1@gmail.com>
2015-03-16 20:45:07 -0600

    hj

--- src/streetFighterSimulation/Stage.java
+++ src/streetFighterSimulation/Stage.java
@@ -4,6 +4,7 @@ import sounds.Music;
 import sounds.SoundClips;
 import charcter.*;
 import enums.Character;
+import enums.Maps;
 import enums.ScaleOfScreen;
 import buttons.MapButton;
 import greenfoot.World;
@@ -12,26 +13,27 @@ public class Stage extends World {
 
 	public static final int WORLD_WIDTH = ScaleOfScreen.WIDTH.getNum();
 	public static final int WORLD_HEIGHT = ScaleOfScreen.HEIGHT.getNum();
-
-	Player dino = new Dragon();
-	Player raptor = new Raptor();
-	HealthBar stat = new HealthBar();
 	
-	private static final int DRAGON_HEALTH = 25;
-	private static final int RAPTOR_HEALTH = 25;
+	public static Maps map = null;
+
+	Player dino;
+	Player raptor;
+	HealthBar stat = new HealthBar();
 
 	public Stage(int worldWidth, int worldHeight, int cellSize, MapButton mapButton) {
 		super(worldWidth, worldHeight, cellSize);
 		setBackground(mapButton,worldHeight,worldWidth);
 		
+		dino = new Dragon();
+		raptor = new Raptor();
+		
 		createPlayers();
 		playAndStopMusic();
 		
-//		dino.setHealth(DRAGON_HEALTH);
 		dino.healthDisplay(dino.getHealth());
 		
-//		raptor.setHealth(RAPTOR_HEALTH);
 		raptor.healthDisplay(raptor.getHealth());
+		
 	}
 
 	private void createPlayers() {
@@ -47,15 +49,19 @@ public class Stage extends World {
 	private void setBackground(MapButton mapButton, int worldHeight, int worldWidth) {
 		switch (mapButton.getNameOfMap()){
 		case "image/Dragon_Stage.gif":
+			map = Maps.DRAGON;
 			this.addObject(new Background("image/Dragon_Stage.gif"),worldWidth/2 , worldHeight/2);
 			break;
 		case "image/Forest-1_Stage.gif":
+			map = Maps.FOREST_TREE;
 			this.addObject(new Background("image/Forest-1_Stage.gif"),worldWidth/2 , worldHeight/2);
 			break;
 		case "image/Forest-0_Stage.gif":
+			map = Maps.FOREST_CLIFF;
 			this.addObject(new Background("image/Forest-0_Stage.gif"),worldWidth/2 , worldHeight/2);
 			break;
 		case "image/Temple_Stage.gif":
+			map = Maps.TEMPLE;
 			this.addObject(new Background("image/Temple_Stage.gif"),worldWidth/2 , worldHeight/2);
 			break;
 
